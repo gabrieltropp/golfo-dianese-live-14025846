214305003becac51304b391e6c36e1f8ed94588b
gabrieltropp <130591101+gabrieltropp@users.noreply.github.com>
2026-08-09 23:03:05 +0200

    git push

--- src/components/cards/WebcamCard.tsx
+++ src/components/cards/WebcamCard.tsx
@@ -21,14 +21,14 @@ const WEBCAMS: Webcam[] = [
   {
     nome: "San Bartolomeo al Mare",
     tipo: "embed",
-    src: "https://webcams.windy.com/webcams/public/embed/player/1591173838/live",
-    pagina: "https://www.ilmeteo.it/webcam/San+Bartolomeo+al+Mare",
+    src: "https://www.windy.com/57bbb963-bbc2-4065-a150-9e4e7609c0d7",
+    pagina: "https://www.twitch.tv/hoteldellerose2021",
   },
   {
-    nome: "Cervo",
-    tipo: "anteprima",
-    thumb: "https://embed.skylinewebcams.com/img/239.jpg",
-    pagina: "https://www.skylinewebcams.com/webcam/italia/liguria/imperia/cervo-ligure.html",
+    nome: "Golfo",
+    tipo: "embed",
+    thumb: "https://www.windy.com/b92170ef-7292-4a37-9c79-c845d347956d",
+    pagina: "https://hoteljasmin.com/webcam/",
   },
 ];
 
