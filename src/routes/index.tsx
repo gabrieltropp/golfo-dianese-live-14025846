f46f5221882ae755de31ab8593fff91e39d415ae
gabrieltropp <130591101+gabrieltropp@users.noreply.github.com>
2026-08-14 14:29:58 +0200

    git push

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -105,7 +105,7 @@ function Index() {
             key={i}
             className="flex w-full items-center justify-center py-4 transition-[min-height,padding] duration-500 ease-out"
             style={{
-             paddingBottom: '20vh',
+             paddingBottom: '5vh',
             }}
           >
             <Reveal index={i} className="w-full" onDone={() => markDone(i)}>
